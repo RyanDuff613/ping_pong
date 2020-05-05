10e4113bd0015588ba5f3ad13987ed8ea55dd975
Ryan Duff <ryanduff613@gmail.com>
2020-05-04 22:34:48 -0700

    write second test, achieve meaningful failure

--- spec/ping_pong_spec.rb
+++ spec/ping_pong_spec.rb
@@ -5,4 +5,8 @@ describe('Integer#ping_pong') do
   it("returns array counting up to given number") do
     expect(ping_pong(2)).to(eq([1,2]))
   end
+
+  it("replaces multiples of three with ping as a string in the array") do
+    expect(ping_pong(3)).to(eq([1,2,"ping"]))
+  end
 end
